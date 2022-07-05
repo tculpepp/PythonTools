baafa2407c616e6284a3a8db191e99cd585e520a
Tom Culpepper <tculpepp@gmail.com>
2022-07-05 12:08:23 -0500

    search and convert to functions

--- tui/TuiDendronConverter.py
+++ tui/TuiDendronConverter.py
@@ -8,6 +8,7 @@ def directory_find(dirName, root='.'):
             return os.path.join(path, dirName)
 # find the modules directory
 modulesDir = directory_find('Modules')
+syllabusDir = directory_find('Syllabus')
 
 # make a temp working dir
 script_temp_dir = "./tui/temp_working"
@@ -16,24 +17,19 @@ os.system("mkdir "+script_temp_dir)
 def html_to_markdown(extension, out_dir):
     for name in files:
         if name.endswith(extension):
-            out_name = name[4:-5]+name[3:4]+".md"
+            if not path.endswith('Syllabus'):
+                out_name = name[4:-5]+name[3:4]+".md"
+            else:
+                out_name = name[:-5]+".md"
             source_full_path = path+"/"+name
             out_full_path = out_dir+"/"+out_name
-            print('Converting '+out_name)
+            print(out_name)
             os.system("pandoc --wrap=none --from html --to markdown_strict "+source_full_path+" -o "+out_full_path)
 
+print('Converting Module Files...')
 for path, dir, files in os.walk(modulesDir):
     html_to_markdown('.html', script_temp_dir)
 
-
-# # this uses os.walk to recursively loop through the dir and find files with ____ extension
-# for path, dir, files in os.walk(modulesDir):
-#     for name in files:
-#         # this renames the files and converts them to markdown with pandoc
-#         if name.endswith('.html'):
-#             # this complex sequence chops up the file name to match the structure
-#             out_name = name[4:-5]+name[3:4]+".md"
-#             print(new_name)
-#             full_file_path=path+"/"+name
-#             full_new_path = script_temp_dir+"/"+out_name
-#             os.system("pandoc --wrap=none --from html --to markdown_strict "+full_file_path+" -o "+full_new_path)
+print('Converting Syllabus Files...')
+for path, dir, files in os.walk(syllabusDir):
+    html_to_markdown('.html', script_temp_dir)

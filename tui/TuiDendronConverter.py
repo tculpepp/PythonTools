13fa2171784d5f7c40a52ad4627b14248222f6f3
Tom Culpepper <tculpepp@gmail.com>
2022-07-05 11:07:49 -0500

    functionized html to md converter

--- tui/TuiDendronConverter.py
+++ tui/TuiDendronConverter.py
@@ -13,14 +13,27 @@ modulesDir = directory_find('Modules')
 script_temp_dir = "./tui/temp_working"
 os.system("mkdir "+script_temp_dir)
 
-# this uses os.walk to recursively loop through the dir and find files with ____ extension
-for path, dir, files in os.walk(modulesDir):
+def html_to_markdown(extension, out_dir):
     for name in files:
-        # this renames the files and converts them to markdown with pandoc
-        if name.endswith('.html'):
-            # this complex sequence chops up the file name to match the structure
-            new_name = name[4:-5]+name[3:4]+".md"
-            print(new_name)
-            full_file_path=path+"/"+name
-            full_new_path = script_temp_dir+"/"+new_name
-            os.system("pandoc --wrap=none --from html --to markdown_strict "+full_file_path+" -o "+full_new_path)
+        if name.endswith(extension):
+            out_name = name[4:-5]+name[3:4]+".md"
+            source_full_path = path+"/"+name
+            out_full_path = out_dir+"/"+out_name
+            print('Converting '+out_name)
+            os.system("pandoc --wrap=none --from html --to markdown_strict "+source_full_path+" -o "+out_full_path)
+
+for path, dir, files in os.walk(modulesDir):
+    html_to_markdown('.html', script_temp_dir)
+
+
+# # this uses os.walk to recursively loop through the dir and find files with ____ extension
+# for path, dir, files in os.walk(modulesDir):
+#     for name in files:
+#         # this renames the files and converts them to markdown with pandoc
+#         if name.endswith('.html'):
+#             # this complex sequence chops up the file name to match the structure
+#             out_name = name[4:-5]+name[3:4]+".md"
+#             print(new_name)
+#             full_file_path=path+"/"+name
+#             full_new_path = script_temp_dir+"/"+out_name
+#             os.system("pandoc --wrap=none --from html --to markdown_strict "+full_file_path+" -o "+full_new_path)

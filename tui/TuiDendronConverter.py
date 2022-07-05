7c55f8a11307a7bb2c603f8c71248b3b23460804
Tom Culpepper <tculpepp@gmail.com>
2022-07-04 20:23:45 -0500

    rename and convert to MD works

--- tui/TuiDendronConverter.py
+++ tui/TuiDendronConverter.py
@@ -9,10 +9,18 @@ def directory_find(dirName, root='.'):
 # find the modules directory
 modulesDir = directory_find('Modules')
 
+# make a temp working dir
+script_temp_dir = "./tui/temp_working"
+os.system("mkdir "+script_temp_dir)
+
 # this uses os.walk to recursively loop through the dir and find files with ____ extension
 for path, dir, files in os.walk(modulesDir):
     for name in files:
+        # this renames the files and converts them to markdown with pandoc
         if name.endswith('.html'):
             # this complex sequence chops up the file name to match the structure
-            new_name = name[4:-5]+name[3:4]+name[-5:]
+            new_name = name[4:-5]+name[3:4]+".md"
             print(new_name)
+            full_file_path=path+"/"+name
+            full_new_path = script_temp_dir+"/"+new_name
+            os.system("pandoc --wrap=none --from html --to markdown_strict "+full_file_path+" -o "+full_new_path)

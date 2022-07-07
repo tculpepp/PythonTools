25ebcaf270359f905e15414b6d74d758db75eb8a
Tom Culpepper <tculpepp@gmail.com>
2022-07-06 21:01:00 -0500

    refactor html_to_markdown and control loop to break-out href_converter

--- tui/TuiDendronConverter.py
+++ tui/TuiDendronConverter.py
@@ -42,34 +42,25 @@ def href_converter(html_file):
         with open(html_file, "w") as file:
             file.write(str(soup))
 
-def html_to_markdown(extension, out_dir):
-    for name in files:
-        if name.endswith(extension):
-            if not path.endswith('Syllabus'):
-                out_name = name[4:-5]+name[3:4]+".md"
-            else:
-                out_name = name[:-5]+".md"
-            source_full_path = path+"/"+name
-            out_full_path = out_dir+"/"+out_name
-            print(out_name)
-            href_converter(source_full_path)
-            
-            os.system("pandoc --wrap=none --from html --to markdown_strict "+source_full_path+" -o "+out_full_path)
-
-
-print('Converting Module Files...')
-for path, dir, files in os.walk(modulesDir):
-    html_to_markdown('.html', script_temp_dir)
-
-print('Converting Syllabus Files...')
-for path, dir, files in os.walk(syllabusDir):
-    html_to_markdown('.html', script_temp_dir)
+def html_to_markdown(out_dir):
+    if not path.endswith('Syllabus'):
+        out_name = name[4:-5]+name[3:4]+".md"
+    else:
+        out_name = name[:-5]+".md"
+    # source_full_path = path+"/"+name
+    out_full_path = out_dir+"/"+out_name
+    print(out_name)
+    # href_converter(source_full_path)
+    
+    os.system("pandoc --wrap=none --from html --to markdown_strict "+source_full_path+" -o "+out_full_path)
 
 dir_list = [('Modules', modulesDir), ('Syllabus', syllabusDir)]
 
 for dir in dir_list:
-    print('Converting '+dir_list+' files...')
-    for path, dir, files in os.walk(dir)
-        source_full_path = path+"/"+file
-        href_converter(source_full_path)
-        html_to_markdown('.html', files)
+    print('Converting '+dir[0]+' files...')
+    for path, dir, files in os.walk(dir[1]):
+        for name in files:
+            if name.endswith('.html'):
+                source_full_path = path+"/"+name
+                href_converter(source_full_path)
+                html_to_markdown(script_temp_dir)
